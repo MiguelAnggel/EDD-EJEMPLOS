4bbbd6d043bc5ef4002ee08dfa65515bb4af1edb
MiguelAnggel <miguelangelmontoya1015@gmail.com>
2024-06-11 15:37:46 -0600

    ejemplo 2 de arreglos

---
+++ arreglosejemplo2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //forma de escribir un arreglo(en este caso con 5 posiciones)
+    int numero[]={4,5,6,7,10,11};
+/*Aqui un ejemplo de recorrer un arreglo (en este caso hasta la
+posicion 5), podemos usar un bucle FOR
+*/
+    for(int i = 0; i<=5; i++){
+        cout<<numero[i]<<endl;
+
+    }
+
+
+    return 0;
+}
