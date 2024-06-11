56f380163f0bbd57f3bad1506e6955e7ee949d33
MiguelAnggel <miguelangelmontoya1015@gmail.com>
2024-06-11 14:42:27 -0600

    Agrego ejemplos c/c++

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //string cars[4];
+  int myNum[3] = {10, 20, 30};
+  string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+cout << cars[0];
+// Outputs Volvo
+  return 0;
+} 
